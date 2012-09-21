f8b3646eb01a208cb0affcf28f3bc4eacf0d4e2e
Keraunos <gaetan.girin.eu@gmail.com>
2012-09-21 12:05:43 +0200

    added a basic PGM file reading feature

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+/**
+ * Main class of TP application
+ * 
+ * @author gaetan
+ */
 public class Main {
 
 	/**
@@ -6,8 +17,32 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		// foo bar
-		// foo bar
+		System.out.println("---------------------");
+		System.out.println("Binary images process");
+		System.out.println("---------------------\n");
+		
+		// create list of image names
+		ArrayList<String> imgNames = new ArrayList<String>();
+        imgNames.add("lena512x512.pgm");
+        imgNames.add("peppers512x512.pgm");
+		
+        // initialize selected image index in imgNames
+        int selectedImgIndex = 1; 
+        
+        // open file
+		File imgFile = new File("resources/img/" + imgNames.get(selectedImgIndex));
+		FileInputStream in;
+		try {
+			in = new FileInputStream(imgFile);
+		} catch (FileNotFoundException e) {
+			System.out.println("Sorry, the file cannot be found.");
+			return;
+		}
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		
+		// read file
+		PgmParser parser = new PgmParser();
+		parser.readPgm(reader);
 		
 	}
 
---
+++ src/PgmParser.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * This class provides utilities to read .pgm files 
+ * 
+ * @author gaetan
+ *
+ */
+public class PgmParser {
+	
+	
+	/**
+	 * Default constructor
+	 */
+	public PgmParser() {
+		super();
+	}
+	
+	
+	/**
+	 * Reads all the lines of a .pgm file
+	 * @param reader
+	 */
+	public void readPgm(BufferedReader reader) {
+		
+		String line = null;
+		try {
+			System.out.println("BEGIN -------------");
+			// read all lines
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+			System.out.println("END ---------------");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
